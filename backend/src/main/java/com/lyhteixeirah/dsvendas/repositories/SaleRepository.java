db916da9d4d47a1d693d428276cfc7b43bc281e6
lyhteixeirah <lielses@live.com>
2021-09-10 01:56:58 -0300

    First homolog

--- backend/src/main/java/com/lyhteixeirah/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/lyhteixeirah/dsvendas/repositories/SaleRepository.java
@@ -2,8 +2,8 @@ package com.lyhteixeirah.dsvendas.repositories;
 
 import java.util.List;
 
-import com.lyhteixeirah.dsvendas.dto.SaleSumDTO;
 import com.lyhteixeirah.dsvendas.dto.SaleSuccessDTO;
+import com.lyhteixeirah.dsvendas.dto.SaleSumDTO;
 import com.lyhteixeirah.dsvendas.entities.Sale;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,10 +12,10 @@ import org.springframework.data.jpa.repository.Query;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     /**Donut Chart*/
-    @Query("SELECT new com.lyhteixeirah.dsvendas.dto.SaleSumDto(obj.seller, SUM(obj.amount)) FROM Sale AS obj GROUP BY obj.seller")
+    @Query("SELECT new com.lyhteixeirah.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) FROM Sale AS obj GROUP BY obj.seller")
     List<SaleSumDTO> amountGroupedBySeller();
 
     /**Bar Chart */
-    @Query("SELECT new com.lyhteixeirah.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals) ) FROM Sale AS obj GROUP BY obj.seller")
+    @Query("SELECT new com.lyhteixeirah.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) FROM Sale AS obj GROUP BY obj.seller")
     List<SaleSuccessDTO> successGroupedBySeller();
 }
